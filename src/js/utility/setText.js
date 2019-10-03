822570f4f5fb5203c17259c303b138fad2d1fec1
Franco Meriles <fran.meriles@gmail.com>
2019-10-02 21:30:52 -0300

    fix some styles add image lines

--- src/js/utility/setText.js
+++ src/js/utility/setText.js
@@ -2,13 +2,13 @@ export const setText = ({ country, keysearch, category, sources }, type) => {
   const titleNode = document.querySelector("#title");
   switch (type) {
     case "one":
-      titleNode.innerHTML = `Lates News from <span>${country}</span>`;
+      titleNode.innerHTML = `Latest News from <span>${country}</span>`;
       break;
     case "two":
-      titleNode.innerHTML = `Lates News from <span>${country}</span> with a category <span>${category}</span>`;
+      titleNode.innerHTML = `Latest News from <span>${country}</span> with a category <span>${category}</span>`;
       break;
     case "three":
-      titleNode.innerHTML = `Lates News from sources <span>${sources}</span> and the keyword search is <span>"${keysearch}"</span>`;
+      titleNode.innerHTML = `Latest News from sources <span>${sources}</span> and the keyword search is <span>"${keysearch}"</span>`;
       break;
   }
 };
