46d0ae84f24bbb5faae2e52b4235598b7ac77801
Franco Meriles <fran.meriles@gmail.com>
2019-11-10 17:17:43 -0300

    fix mobile stlyes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import "bootstrap";
 import "./sass/main.scss";
 
-// JS files
-
+// Main file app
 import "./js/main";
 
-//IIFE TEST ASYNC AWAIT
-(async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-  const data = await response.json();
-  //console.table(data);
-})();
